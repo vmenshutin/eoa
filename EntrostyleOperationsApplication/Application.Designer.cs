64be66cd9050c8d5b33c2087da3d371208c8a33a
FMonkey <FMonkey@DESKTOP-LD5LNGV>
2016-07-18 16:24:02 -0400

    Add project files.

---
+++ EntrostyleOperationsApplication/Application.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace EntrostyleOperationsApplication
+{
+    partial class Application
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.ScheduleTab = new System.Windows.Forms.TabPage();
+            this.SOSecondary = new System.Windows.Forms.DataGridView();
+            this.SOMain = new System.Windows.Forms.DataGridView();
+            this.DifotTab = new System.Windows.Forms.TabPage();
+            this.SettingsTab = new System.Windows.Forms.TabPage();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.ScheduleTab.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.SOSecondary)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SOMain)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.ScheduleTab);
+            this.tabControl1.Controls.Add(this.DifotTab);
+            this.tabControl1.Controls.Add(this.SettingsTab);
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1425, 735);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // ScheduleTab
+            // 
+            this.ScheduleTab.Controls.Add(this.label11);
+            this.ScheduleTab.Controls.Add(this.label10);
+            this.ScheduleTab.Controls.Add(this.label9);
+            this.ScheduleTab.Controls.Add(this.label8);
+            this.ScheduleTab.Controls.Add(this.label7);
+            this.ScheduleTab.Controls.Add(this.label6);
+            this.ScheduleTab.Controls.Add(this.label5);
+            this.ScheduleTab.Controls.Add(this.label4);
+            this.ScheduleTab.Controls.Add(this.label3);
+            this.ScheduleTab.Controls.Add(this.label2);
+            this.ScheduleTab.Controls.Add(this.label1);
+            this.ScheduleTab.Controls.Add(this.SOSecondary);
+            this.ScheduleTab.Controls.Add(this.SOMain);
+            this.ScheduleTab.Location = new System.Drawing.Point(4, 22);
+            this.ScheduleTab.Name = "ScheduleTab";
+            this.ScheduleTab.Padding = new System.Windows.Forms.Padding(3);
+            this.ScheduleTab.Size = new System.Drawing.Size(1417, 709);
+            this.ScheduleTab.TabIndex = 0;
+            this.ScheduleTab.Text = "Schedule";
+            this.ScheduleTab.UseVisualStyleBackColor = true;
+            // 
+            // SOSecondary
+            // 
+            this.SOSecondary.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.SOSecondary.Location = new System.Drawing.Point(105, 326);
+            this.SOSecondary.Name = "SOSecondary";
+            this.SOSecondary.Size = new System.Drawing.Size(914, 360);
+            this.SOSecondary.TabIndex = 1;
+            // 
+            // SOMain
+            // 
+            this.SOMain.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.SOMain.Location = new System.Drawing.Point(105, 6);
+            this.SOMain.Name = "SOMain";
+            this.SOMain.Size = new System.Drawing.Size(914, 302);
+            this.SOMain.TabIndex = 0;
+            // 
+            // DifotTab
+            // 
+            this.DifotTab.Location = new System.Drawing.Point(4, 22);
+            this.DifotTab.Name = "DifotTab";
+            this.DifotTab.Padding = new System.Windows.Forms.Padding(3);
+            this.DifotTab.Size = new System.Drawing.Size(1417, 709);
+            this.DifotTab.TabIndex = 1;
+            this.DifotTab.Text = "DIFOT";
+            this.DifotTab.UseVisualStyleBackColor = true;
+            // 
+            // SettingsTab
+            // 
+            this.SettingsTab.Location = new System.Drawing.Point(4, 22);
+            this.SettingsTab.Name = "SettingsTab";
+            this.SettingsTab.Size = new System.Drawing.Size(1417, 709);
+            this.SettingsTab.TabIndex = 2;
+            this.SettingsTab.Text = "Settings";
+            this.SettingsTab.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1035, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "#:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1035, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Account:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(1037, 65);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Address:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(1037, 111);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(84, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Last Scheduled:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(1037, 135);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(42, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "DIFOT:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(1136, 18);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(14, 13);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "#";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(1136, 41);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(47, 13);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "Account";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(1136, 65);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(51, 13);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "Address1";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(1136, 89);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(51, 13);
+            this.label9.TabIndex = 10;
+            this.label9.Text = "Address2";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(1136, 111);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(81, 13);
+            this.label10.TabIndex = 11;
+            this.label10.Text = "Last Scheduled";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(1136, 135);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(39, 13);
+            this.label11.TabIndex = 12;
+            this.label11.Text = "DIFOT";
+            // 
+            // Application
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1425, 734);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Application";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.ScheduleTab.ResumeLayout(false);
+            this.ScheduleTab.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.SOSecondary)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SOMain)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage ScheduleTab;
+        private System.Windows.Forms.DataGridView SOMain;
+        private System.Windows.Forms.TabPage DifotTab;
+        private System.Windows.Forms.TabPage SettingsTab;
+        private System.Windows.Forms.DataGridView SOSecondary;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ EntrostyleOperationsApplication/Application.cs
@@ -0,0 +1,299 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+using System.Data.Odbc;
+using System.Data;
+using System.Drawing;
+
+namespace EntrostyleOperationsApplication
+{
+    public partial class Application : Form
+    {
+
+        int sessionId;
+        OdbcConnection connection = new OdbcConnection();
+
+        OdbcDataAdapter SOMainAdapter;
+        DataSet SOMainDataSet = new DataSet();
+
+        OdbcDataAdapter SOSecondaryAdapter;
+        DataSet SOSecondaryDataSet = new DataSet();
+
+        public Application()
+        {
+            InitializeComponent();
+
+            // read connection string and open the connection
+            connection.ConnectionString = readConfig();
+            connection.Open();
+
+            // generate temporary session id and make sure it is unique
+            sessionId = generateSessionId();
+            verifyUserIsUnique();
+
+            // load main and secondary sales orders grids
+            loadSalesOrdersMain();
+            loadSalesOrdersSecondary();
+
+            // add fake dropdown columns for dispatch status and method
+            addStatusAndMethodDropdowns(SOMain);
+            addStatusAndMethodDropdowns(SOSecondary);
+
+            // set basic grid styling
+            setDataGridViewStyleProps(SOMain);
+            setDataGridViewStyleProps(SOSecondary);
+
+            // customize columns
+            styleDataGridViewColumns(SOMain);
+            styleDataGridViewColumns(SOSecondary);
+
+            // set up action listeners
+            setDataGridViewListeners();
+
+            // focus main grid or secondary if main has 0 rows
+            focusSO();
+        }
+
+        // reads config txt
+        private string readConfig()
+        {
+            using (StreamReader sr = new StreamReader("config.txt"))
+            {
+                string connectionString = sr.ReadToEnd();
+                int serverNamePrefixIndex = connectionString.IndexOf("Data Source=");
+                int serverNameIndex = serverNamePrefixIndex + 12;
+                int databaseNamePrefixIndex = connectionString.IndexOf(";Initial Catalog=");
+
+                string serverName = connectionString.Substring(serverNameIndex, databaseNamePrefixIndex - serverNameIndex);
+
+                int databaseNameIndex = databaseNamePrefixIndex + 17;
+                int securityPrefixIndex = connectionString.IndexOf(";Persist Security Info=");
+
+                string databaseName = connectionString.Substring(databaseNameIndex, securityPrefixIndex - databaseNameIndex);
+
+                int loginPrefixIndex = connectionString.IndexOf("User ID=");
+                int loginIndex = loginPrefixIndex + 8;
+                int passwordPrefixIndex = connectionString.IndexOf(";Password=");
+
+                string loginName = connectionString.Substring(loginIndex, passwordPrefixIndex - loginIndex);
+
+                int passwordIndex = passwordPrefixIndex + 10;
+                int passwordEndIndex = connectionString.IndexOf("\"\r\n\r\n#");
+
+                string passwordName = connectionString.Substring(passwordIndex, passwordEndIndex - passwordIndex);
+
+                return "Driver={SQL Server};Server=" + serverName + ";UID=" + loginName + ";PWD=" + passwordName + ";Database=" + databaseName + ";";
+            }
+        }
+
+        // focus main grid or secondary if main has 0 rows
+        private void focusSO()
+        {
+            if (SOMain.Rows.Count > 0)
+            {
+                SOMain.Rows[0].Selected = true;
+            }
+            else
+            {
+                SOSecondary.Rows[0].Selected = true;
+            }
+        }
+
+        // Generates randon int number for unique session id
+        private int generateSessionId()
+        {
+            return (new Random()).Next(0, 9999);
+        }
+
+        // loads data to main sales orders table
+        private void loadSalesOrdersMain()
+        {
+            (new OdbcCommand("exec query_salesorders_main " + sessionId.ToString(), connection)).ExecuteNonQuery();
+
+            SOMainAdapter = new OdbcDataAdapter("SELECT * FROM EOA_SALESORD_MAIN where SESSIONID = " + sessionId.ToString(), connection);
+            OdbcCommandBuilder cmdbuilder = new OdbcCommandBuilder(SOMainAdapter);
+            SOMainAdapter.Fill(SOMainDataSet);
+
+            SOMain.DataSource = SOMainDataSet.Tables[0];
+        }
+
+        // loads data to main sales orders table
+        private void loadSalesOrdersSecondary()
+        {
+            (new OdbcCommand("exec query_salesorders_secondary " + sessionId.ToString(), connection)).ExecuteNonQuery();
+
+            SOSecondaryAdapter = new OdbcDataAdapter("SELECT * FROM EOA_SALESORD_SECONDARY where SESSIONID = " + sessionId.ToString(), connection);
+            OdbcCommandBuilder cmdbuilder = new OdbcCommandBuilder(SOSecondaryAdapter);
+            SOSecondaryAdapter.Fill(SOSecondaryDataSet);
+
+            SOSecondary.DataSource = SOSecondaryDataSet.Tables[0];
+        }
+
+        // clear obsolete sessions data and verify current session id is unique
+        private void verifyUserIsUnique()
+        {
+            (new OdbcCommand("exec clear_obsolete_sessions", connection)).ExecuteNonQuery();
+
+            try
+            {
+                (new OdbcCommand("exec eoa_authentificate " + sessionId.ToString(), connection)).ExecuteNonQuery();
+            }
+            catch
+            {
+                MessageBox.Show("Authentification failed. Please, restart the application");
+            }
+        }
+
+        // add dispatch status and method dropdowns
+        private void addStatusAndMethodDropdowns(DataGridView dgv)
+        {
+            var dispatchStatusColumn = new DataGridViewComboBoxColumn();
+            dispatchStatusColumn.HeaderText = "Status";
+            dispatchStatusColumn.Name = "STATUS_FAKE";
+
+
+            var listSource = new string[] { "TP", "W", "P", "TA", "Sc", "Sh" };
+            dispatchStatusColumn.DataSource = listSource;
+            dispatchStatusColumn.DisplayStyle = DataGridViewComboBoxDisplayStyle.Nothing;
+
+            dgv.Columns.Add(dispatchStatusColumn);
+            dgv.Columns[dispatchStatusColumn.Name].DataPropertyName = "STATUS";
+
+            //----------------------METHOD-----------------------
+
+            var dispatchMethodColumn = new DataGridViewComboBoxColumn();
+            dispatchMethodColumn.HeaderText = "Method";
+            dispatchMethodColumn.Name = "METHOD_FAKE";
+
+            var methodSource = new string[] { "E4", "E4c", "G", "P", "I", "(n)" };
+            dispatchMethodColumn.DataSource = methodSource;
+            dispatchMethodColumn.DisplayStyle = DataGridViewComboBoxDisplayStyle.Nothing;
+
+            dgv.Columns.Add(dispatchMethodColumn);
+            dgv.Columns[dispatchMethodColumn.Name].DataPropertyName = "METHOD";
+        }
+
+        // style Data Grid View columns
+        private void styleDataGridViewColumns(DataGridView dgv)
+        {
+            var columns = dgv.Columns;
+
+            foreach (DataGridViewColumn column in columns)
+            {
+                if (column.Name != "#"
+                    && column.Name != "ACCOUNTNAME"
+                    && column.Name != "STOCK")
+                {
+                    column.DefaultCellStyle.BackColor = Color.LightGray;
+                }
+                else
+                {
+                    column.ReadOnly = true;
+                }
+            }
+
+            columns["STATUS"].Visible = false;
+            columns["METHOD"].Visible = false;
+            columns["SESSIONID"].Visible = false;
+            columns["DIFOT_TIMESTAMP"].Visible = false;
+            columns["LAST_SCHEDULED"].Visible = false;
+            columns["ADDRESS1"].Visible = false;
+            columns["ADDRESS2"].Visible = false;
+        }
+
+        // set major style properties for datagridview
+        private void setDataGridViewStyleProps(DataGridView dgv)
+        {
+            dgv.EditMode = DataGridViewEditMode.EditOnEnter;
+            dgv.CellBorderStyle = DataGridViewCellBorderStyle.Raised;
+            dgv.MultiSelect = true;
+            dgv.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dgv.DefaultCellStyle.SelectionBackColor = Color.White;
+            dgv.DefaultCellStyle.SelectionForeColor = Color.Black;
+            dgv.DefaultCellStyle.Font = new Font("Arial", 11F, FontStyle.Regular, GraphicsUnit.Pixel);
+            dgv.BorderStyle = BorderStyle.Fixed3D;
+            dgv.RowHeadersVisible = false;
+            dgv.AllowUserToAddRows = false;
+            dgv.AllowUserToResizeRows = false;
+            dgv.AllowUserToDeleteRows = false;
+
+            //Disable custom column sorting for all splits
+            foreach (DataGridViewColumn column in dgv.Columns)
+            {
+                column.SortMode = DataGridViewColumnSortMode.NotSortable;
+            }
+        }
+
+        // set listeners to datagridviews
+        private void setDataGridViewListeners()
+        {
+            // save values to database with each call value change
+            SOMain.CellValueChanged += SO_CellValueChanged;
+            SOSecondary.CellValueChanged += SO_CellValueChanged;
+
+            // react when selected row changed
+            SOMain.RowEnter += SO_RowEnter;
+            SOSecondary.RowEnter += SO_RowEnter;
+        }
+
+        // react when selected row changed
+        private void SO_RowEnter(object sender, DataGridViewCellEventArgs e)
+        {
+            var selectedRows = ((DataGridView)sender).SelectedRows;
+
+            if (selectedRows.Count > 0)
+            {
+                var cells = selectedRows[0].Cells;
+
+                label6.Text = cells["#"].Value.ToString();
+                label7.Text = cells["ACCOUNTNAME"].Value.ToString();
+                label8.Text = cells["ADDRESS1"].Value.ToString();
+                label9.Text = cells["ADDRESS2"].Value.ToString();
+                label10.Text = cells["LAST_SCHEDULED"].Value.ToString();
+                label11.Text = cells["DIFOT_TIMESTAMP"].Value.ToString();
+            }
+        }
+
+        // updates database when 
+        private void SO_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            Validate();
+
+            // which grid to update
+            var dgv = (DataGridView)sender;
+
+            // which column value has been changed
+            string columnName = dgv.Columns[e.ColumnIndex].Name;
+
+            // is column fake?
+            int index = columnName.IndexOf("_FAKE");
+
+            // remove _FAKE part of column name if it exists
+            string cleanColumnName = (index < 0)
+                ? columnName
+                : columnName.Remove(index, 5);
+
+            // set corresponding data adapters and data sets
+            var adapter = dgv.Name == "SOMain" ? SOMainAdapter : SOSecondaryAdapter;
+            var dataset = dgv.Name == "SOMain" ? SOMainDataSet : SOSecondaryDataSet;
+
+            // set corresponding stored procedure name prefix
+            string procedureNamePrefix = dgv.Name == "SOMain" ? "exec so_main_update_" : "exec so_secondary_update_";
+
+            try
+            {
+                adapter.Update(dataset);
+            }
+            catch (DBConcurrencyException)
+            {
+                MessageBox.Show("Concurrency violation! Please, refresh the app.");
+            }
+
+            // run stored procedure and update real database tables
+            (new OdbcCommand(procedureNamePrefix
+               + cleanColumnName + " "
+               + sessionId.ToString() + ", "
+               + dgv.Rows[e.RowIndex].Cells["#"].Value.ToString(), connection)).ExecuteNonQuery();
+        }
+    }
+}
