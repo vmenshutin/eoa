6be4022d9cc608239ed73867c762c21ae585ff4d
FMonkey <FMonkey@DESKTOP-LD5LNGV>
2018-06-07 11:49:24 -0400

    updated version;

--- EntrostyleOperationsApplication/Application.Designer.cs
+++ EntrostyleOperationsApplication/Application.Designer.cs
@@ -1044,7 +1044,7 @@
             this.label20.Name = "label20";
             this.label20.Size = new System.Drawing.Size(117, 13);
             this.label20.TabIndex = 57;
-            this.label20.Text = "EOA 1.8.1 25/05/2018";
+            this.label20.Text = "EOA 1.8.3 07/06/2018";
             // 
             // label18
             // 
